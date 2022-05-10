026766fbb4c986c42ebe542c48101f360a8406e7
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-10 22:11:41 +0300

    Added 3.4.3

---
+++ 3.4.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _3._4._3
+{
+    class Program
+    {
+        enum Semafore
+        {
+            Red = 100,
+            Yellow = 200,
+            Green = 300
+        }
+    }
+}
