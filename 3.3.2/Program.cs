8386508374d59eafdaaa6baec15969a103693aff
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-10 22:07:43 +0300

    Added 3.3.2

--- 3.3.2/Program.cs
+++ 3.3.2/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace _3._3._2
+class MainClass
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	public static void Main(string[] args)
+	{
+
+		string MyName = "Jane";
+		byte MyAge = 27;
+		bool HaveIApet = true;
+		double MyShoeSize = 37.5;
+
+		Console.WriteLine("My name is " + MyName);
+		Console.WriteLine("MyAge " + MyAge);
+		Console.WriteLine("Do I have a pet? " + HaveIApet);
+		Console.WriteLine("My shoe size is " + MyShoeSize);
+
+	}
 }
