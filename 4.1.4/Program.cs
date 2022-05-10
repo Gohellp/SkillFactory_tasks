b90db9acff41601790f1d8f4e5150695a4ef46ee
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-10 22:34:53 +0300

    Added 4.1.4 and 4.1.5

---
+++ 4.1.4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _4._1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A;
+            string B;
+            bool C = A != B;
+        }
+    }
+}
---
+++ 4.1.5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _4._1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            double X, Y;
+            bool C = A < B | X > Y;
+        }
+    }
+}
