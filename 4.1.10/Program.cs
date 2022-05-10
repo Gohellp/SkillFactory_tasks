5ec2e4589b8ef408c9cc21f90ae4bdb6283436bf
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-10 22:37:18 +0300

    Added 4.1.10

---
+++ 4.1.10/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        var inv = true;
+        var result = !inv;
+        Console.WriteLine(result);
+    }
+}
