985ce113b534995be6aacc6a53f7d7958397d553
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-10 23:06:42 +0300

    Added 4.3.7

---
+++ 4.3.7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _4._3._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            string name_rev="";
+            for(int i = name.Length-1; i >= 0; i--)
+            {
+                name_rev += name[i];
+            }
+            Console.WriteLine(name_rev);
+        }
+    }
+}
