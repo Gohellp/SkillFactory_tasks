4317e0fa111460fa796ead74d4c76db97839dff6
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-16 02:43:24 +0300

    Added 4.3.16

---
+++ 4.3.16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4._3._16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+            int count = 0;
+
+            foreach(var num in arr)
+            {
+                if (num > 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Pos. number count is: {0}", count);
+        }
+    }
+}
