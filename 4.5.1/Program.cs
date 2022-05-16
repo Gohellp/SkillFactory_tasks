a27fdfb97b8cd2117b0db31ab73a45e01b2820f0
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-16 03:26:36 +0300

    Done 4.5.1

---
+++ 4.5.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4._5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, string login, string count_login, bool has_pet, int age, string[] fav_colors) User;
+        }
+    }
+}
