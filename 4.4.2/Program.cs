9c1051c7c63225a0baab3e8f65af046e12ee0303
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-16 03:05:57 +0300

    Added 4.4.2

---
+++ 4.4.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4._4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, int age) form;
+            Console.Write("Enter your name: ");
+            form.name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            form.age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1} ", form.name, form.age);
+        }
+    }
+}
