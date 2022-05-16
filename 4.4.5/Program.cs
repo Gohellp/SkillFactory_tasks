228deedc74e9a0c506988d63fd286f4d59647e1d
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-16 03:22:04 +0300

    Added 4.4.5

---
+++ 4.4.5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4._4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string Type, double Age, int NameCount) Pet;
+
+            Console.Write("Enter you'r pes's name: ");
+            Pet.Name = Console.ReadLine();
+            Console.Write("Enter you'r pes's type: ");
+            Pet.Type = Console.ReadLine();
+            Console.Write("Enter you'r pet's age: ");
+            Pet.Age = double.Parse(Console.ReadLine());
+            Pet.NameCount = Pet.Name.Length;
+        }
+    }
+}
