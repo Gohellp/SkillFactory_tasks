bb1bf468524341b61288b4e3dc9ca1eb93b3147a
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-16 03:40:26 +0300

    Done 4.5.4

--- 4.5.x/Program.cs
+++ 4.5.x/Program.cs
@@ -8,6 +8,7 @@ namespace _4._5.x
         {
             (string name, string last_name, string login, int login_count, bool has_pet, int age, string[] fav_colors) User;
 
+
             Console.Write("Enter you'r name: ");
             User.name = Console.ReadLine();
 
@@ -17,6 +18,27 @@ namespace _4._5.x
             Console.Write("Enter you'r login: ");
             User.login = Console.ReadLine();
             User.login_count = User.login.Length;
+
+            Console.Write("Do you have a pet? ");
+            string temp_has_pet = Console.ReadLine();
+            switch (temp_has_pet.ToLower())
+            {
+                case "да":
+                    User.has_pet = true;
+                    break;
+                case "д":
+                    User.has_pet = true;
+                    break;
+                case "yes":
+                    User.has_pet = true;
+                    break;
+                case "y":
+                    User.has_pet = true;
+                    break;
+                default:
+                    User.has_pet = false;
+                    break;
+            }
         }
     }
 }
