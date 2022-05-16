ddadfb042e4d8e314664e04f8d79bfcb6d317ed1
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-16 03:33:18 +0300

    Done 4.5.2

--- 4.5.x/Program.cs
+++ 4.5.x/Program.cs
@@ -6,7 +6,16 @@ namespace _4._5.x
     {
         static void Main(string[] args)
         {
-            (string name, string login, string count_login, bool has_pet, int age, string[] fav_colors) User;
+            (string name, string last_name, string login, string count_login, bool has_pet, int age, string[] fav_colors) User;
+
+            Console.Write("Enter you'r name: ");
+            User.name = Console.ReadLine();
+
+            Console.Write("Enter you'r last name: ");
+            User.last_name = Console.ReadLine();
+
+            Console.Write("Enter you'r login: ");
+            User.login = Console.ReadLine();
         }
     }
 }
