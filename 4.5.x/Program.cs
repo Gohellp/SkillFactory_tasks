8086d675473a0de5d791d0b66e79403d59711f0b
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-16 03:48:31 +0300

    Done 4.5.6

--- 4.5.x/Program.cs
+++ 4.5.x/Program.cs
@@ -9,19 +9,20 @@ namespace _4._5.x
             (string name, string last_name, string login, int login_count, bool has_pet, int age, string[] fav_colors) User;
 
 
-            Console.Write("Enter your name: ");
-            User.name = Console.ReadLine();
+            for(int j=0;j<3;j++){
+                Console.Write("Enter your name: ");
+                User.name = Console.ReadLine();
 
-            Console.Write("Enter you'r last name: ");
-            User.last_name = Console.ReadLine();
+                Console.Write("Enter you'r last name: ");
+                User.last_name = Console.ReadLine();
 
-            Console.Write("Enter your login: ");
-            User.login = Console.ReadLine();
-            User.login_count = User.login.Length;
+                Console.Write("Enter your login: ");
+                User.login = Console.ReadLine();
+                User.login_count = User.login.Length;
 
-            Console.Write("Do you have a pet? ");
-            string temp_has_pet = Console.ReadLine();
-            switch (temp_has_pet.ToLower())
+                Console.Write("Do you have a pet? ");
+                string temp_has_pet = Console.ReadLine();
+                switch (temp_has_pet.ToLower())
             {
                 case "да":
                     User.has_pet = true;
@@ -40,13 +41,14 @@ namespace _4._5.x
                     break;
             }
 
-            Console.Write("Enter yorr age: ");
-            User.age= checked((byte)int.Parse(Console.ReadLine()));
+                Console.Write("Enter yorr age: ");
+                User.age = checked((byte)int.Parse(Console.ReadLine()));
 
-            User.fav_colors = new string[3];
-            Console.WriteLine("Enter 3 of your favotite colors:");
-            for (int i = 0; i < User.fav_colors.Length; i++)
-                User.fav_colors[i] = Console.ReadLine();
+                User.fav_colors = new string[3];
+                Console.WriteLine("Enter 3 of your favotite colors:");
+                for (int i = 0; i < User.fav_colors.Length; i++)
+                    User.fav_colors[i] = Console.ReadLine();
+            }
         }
     }
 }
