2b2c790891fcfd020740aa9c91a4c851f6e8a721
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-18 00:28:35 +0300

    Added 5.1.5 and removed 5.1.1 to 5.task_1

---
+++ 5.1.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _5._1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] favcolors = new string[3];
+
+            static string ShowColor()
+            {
+                return Console.ReadLine();
+            }
+
+            Console.WriteLine("Eter 3 of your fav color:");
+            for (int i = 0; i < 3; i++)
+                favcolors[i] = ShowColor();
+        }
+    }
+}
