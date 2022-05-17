7cceddc60ae81763c0d07229094ca0825576cced
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-18 00:17:50 +0300

    Added 5.1.1.

---
+++ 5.1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string[] Dishes) User;
+            User.Dishes = new string[5];
+
+            Console.Write("Enter your name: ");
+            User.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter your five fav dishes:");
+            for (int i = 0; i < 5; i++)
+                User.Dishes[i] = Console.ReadLine();
+        }
+    }
+}
