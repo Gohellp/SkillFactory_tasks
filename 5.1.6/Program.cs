2b63780776c8129e1b0893bf147e8dd5d8d6c1be
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-05-18 00:43:42 +0300

    Added 5.1.6

---
+++ 5.1.6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _5._1._6
+{
+    class Program
+    {
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = GetArrayFromConsole();
+            int temp;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
