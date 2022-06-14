c8afc7524aa2379c8ee27a14ad4ae098b2367126
gohellp <47943965+Gohellp@users.noreply.github.com>
2022-06-14 05:54:02 +0300

    Added 5.2.18

---
+++ 5.2.18/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _5._2._18
+{
+    class Program
+    {
+        static int[] GetArrayFromConsole(int num = 5)
+        {
+            var result = new int[num];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Inter arr's elemen #{0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+        static int[] ShowArray(int[] arr, bool must_sort = false)
+        {
+            if (must_sort)
+            {
+                arr = SortArray(arr);
+            }
+
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        static int[] SortArray(int[] arr)
+        {
+            int temp;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+        static void Main(string[] args)
+        {
+            ShowArray(GetArrayFromConsole(10),true);
+        }
+    }
+}
